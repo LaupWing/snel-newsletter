55e16a3208c64eb9f878c7576b33e7aa0e5f4192
laupwing <laupwing@gmail.com>
2026-05-04 18:26:10 +0700

    Track vendor/ in repo to avoid composer install requirement on deploy

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'snelstack/snel-newsletter',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3d4ce1997dde290fe078bbf26bb638b53ae73c05',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'snelstack/snel-newsletter' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3d4ce1997dde290fe078bbf26bb638b53ae73c05',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v5.6',
+            'version' => '5.6.0.0',
+            'reference' => 'a2db6871deec989a74e1f90fafc6d58ae526a879',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
